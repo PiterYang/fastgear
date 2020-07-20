5c49bf8dbd756f9f1629ea4087180e41c16a13ec
tao.yang <tao.yang@ihandysoft.com>
2020-07-20 14:59:23 +0800

    修改 bable

--- vue-component/babel.config.js
+++ vue-component/babel.config.js
@@ -1,14 +1,5 @@
 module.exports = {
-  plugins: [
-    "@babel/plugin-proposal-object-rest-spread",
-    "@babel/plugin-transform-runtime"
-  ],
   presets: [
-    [
-      "@babel/preset-env",
-      {
-        modules: false
-      }
-    ]
+    '@vue/cli-plugin-babel/preset'
   ]
-};
+}
